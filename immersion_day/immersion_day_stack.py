977ffbd0fdef8c60dc83c7f424cbf4909390dd90
Marin Mouscadet <mmouscad@amazon.com>
2022-10-02 22:10:05 +0200

    initial commit

--- immersion_day/immersion_day_stack.py
+++ immersion_day/immersion_day_stack.py
@@ -1,19 +1,23 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    CfnOutput,
+    RemovalPolicy,
+    Stack
 )
 from constructs import Construct
+from aws_solutions_constructs.aws_cloudfront_s3 import CloudFrontToS3
 
 class ImmersionDayStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        
+         # first we dploy an s3 bucket with its cloudfront distribution and its logging facilities ()
+        myCloudFrontToS3 = CloudFrontToS3(
+            self, 'Icare', insert_http_security_headers=False)
 
-        # The code that defines your stack goes here
+        myCloudFrontToS3.s3_bucket.apply_removal_policy(RemovalPolicy.DESTROY)
+        myCloudFrontToS3.cloud_front_logging_bucket.apply_removal_policy(RemovalPolicy.DESTROY)
+        myCloudFrontToS3.s3_logging_bucket.apply_removal_policy(RemovalPolicy.DESTROY)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ImmersionDayQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+
+        CfnOutput(self, "CloudFrontDistribution", value=myCloudFrontToS3.cloud_front_web_distribution.distribution_domain_name)
